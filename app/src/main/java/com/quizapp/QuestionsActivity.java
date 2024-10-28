1048f3ad3845dab97d4c2e4003bb50f8abf1868b
Saadmehmood1234 <158064428+Saadmehmood1234@users.noreply.github.com>
2024-10-28 23:37:37 +0530

    added

--- app/src/main/java/com/quizapp/QuestionsActivity.java
+++ app/src/main/java/com/quizapp/QuestionsActivity.java
@@ -31,23 +31,23 @@ import java.util.ArrayList;
 import java.util.concurrent.TimeUnit;
 
 public class QuestionsActivity extends AppCompatActivity {
-ActivityQuestionsBinding binding;
-FirebaseDatabase database;
-ArrayList<QuestionsModel>list;
-private int count=0;
-private  int position=0;
-private int correctAnsw=0;
-private int wrongAnsw=0;
-private long questionTime=0;
-private long timeLeft;
-private String catId,subCatId;
-CountDownTimer timer;
-private Dialog loadingDialog;
+    ActivityQuestionsBinding binding;
+    FirebaseDatabase database;
+    ArrayList<QuestionsModel>list;
+    private int count=0;
+    private  int position=0;
+    private int correctAnsw=0;
+    private int wrongAnsw=0;
+    private long questionTime=10;
+    private long timeLeft;
+    private String catId,subCatId;
+    CountDownTimer timer;
+    private Dialog loadingDialog;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-       binding=ActivityQuestionsBinding.inflate(getLayoutInflater());
-        setContentView(R.layout.activity_questions);
+        binding = ActivityQuestionsBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
         catId=getIntent().getStringExtra("catId");
         subCatId=getIntent().getStringExtra("subCatId");
         database=FirebaseDatabase.getInstance();
@@ -62,66 +62,66 @@ private Dialog loadingDialog;
                 .child(subCatId).child("questions").addValueEventListener(new ValueEventListener() {
                     @Override
                     public void onDataChange(@NonNull DataSnapshot snapshot) {
-                       if(snapshot.exists()){
-                           timer.start();
-                           for (DataSnapshot dataSnapshot:snapshot.getChildren()){
-                               QuestionsModel model=dataSnapshot.getValue(QuestionsModel.class);
-                               model.setKey(dataSnapshot.getKey());
-                               list.add(model);
-                               loadingDialog.dismiss();
-                           }
-                           if(list.size()>0){
-                               for(int i=0;i<4;i++){
-                                  binding.optionContainer.getChildAt(i).setOnClickListener(new View.OnClickListener() {
-                                      @Override
-                                      public void onClick(View view) {
-                                          checkAnswer((Button)view);
-                                      }
-                                  });
-                               }
-                           }
+                        if(snapshot.exists()){
+                            timer.start();
+                            for (DataSnapshot dataSnapshot:snapshot.getChildren()){
+                                QuestionsModel model=dataSnapshot.getValue(QuestionsModel.class);
+                                model.setKey(dataSnapshot.getKey());
+                                list.add(model);
+                                loadingDialog.dismiss();
+                            }
+                            if(list.size()>0){
+                                for(int i=0;i<4;i++){
+                                    binding.optionContainer.getChildAt(i).setOnClickListener(new View.OnClickListener() {
+                                        @Override
+                                        public void onClick(View view) {
+                                            checkAnswer((Button)view);
+                                        }
+                                    });
+                                }
+                            }
 
-                           playAnimation(binding.question,0,list.get(position).getQuestion());
-                           binding.btnNext.setOnClickListener(new View.OnClickListener() {
-                               @Override
-                               public void onClick(View view) {
-                                   enableOption(true);
-                                   position++;
-                                   if(position==list.size()){
-                                       timer.cancel();
-                                       long totalTime=questionTime*60*100;
-                                       Intent intent=new Intent(QuestionsActivity.this,ScoreActivity.class);
-                                       intent.putExtra("time_taken",totalTime-timeLeft);
-                                       intent.putExtra("correct",correctAnsw);
-                                       intent.putExtra("wrong",wrongAnsw);
-                                       intent.putExtra("total_questions",list.size());
-                                       startActivity(intent);
-                                       finish();
-                                       return;
-                                   }
-                                   count=0;
-                                   playAnimation(binding.question,0,list.get(position).getQuestion());
-                               }
-                           });
-                           binding.btnSubmit.setOnClickListener(new View.OnClickListener() {
-                               @Override
-                               public void onClick(View view) {
-                                   timer.cancel();
-                                   long totalTime=questionTime*60*100;
-                                   Intent intent=new Intent(QuestionsActivity.this,ScoreActivity.class);
-                                   intent.putExtra("time_taken",totalTime-timeLeft);
-                                   intent.putExtra("correct",correctAnsw);
-                                   intent.putExtra("wrong",wrongAnsw);
-                                   intent.putExtra("total_questions",list.size());
-                                   startActivity(intent);
-                               }
-                           });
+                            playAnimation(binding.question,0,list.get(position).getQuestion());
+                            binding.btnNext.setOnClickListener(new View.OnClickListener() {
+                                @Override
+                                public void onClick(View view) {
+                                    enableOption(true);
+                                    position++;
+                                    if(position==list.size()){
+                                        timer.cancel();
+                                        long totalTime=questionTime*60*100;
+                                        Intent intent=new Intent(QuestionsActivity.this,ScoreActivity.class);
+                                        intent.putExtra("time_taken",totalTime-timeLeft);
+                                        intent.putExtra("correct",correctAnsw);
+                                        intent.putExtra("wrong",wrongAnsw);
+                                        intent.putExtra("total_questions",list.size());
+                                        startActivity(intent);
+                                        finish();
+                                        return;
+                                    }
+                                    count=0;
+                                    playAnimation(binding.question,0,list.get(position).getQuestion());
+                                }
+                            });
+                            binding.btnSubmit.setOnClickListener(new View.OnClickListener() {
+                                @Override
+                                public void onClick(View view) {
+                                    timer.cancel();
+                                    long totalTime=questionTime*60*100;
+                                    Intent intent=new Intent(QuestionsActivity.this,ScoreActivity.class);
+                                    intent.putExtra("time_taken",totalTime-timeLeft);
+                                    intent.putExtra("correct",correctAnsw);
+                                    intent.putExtra("wrong",wrongAnsw);
+                                    intent.putExtra("total_questions",list.size());
+                                    startActivity(intent);
+                                }
+                            });
 
-                       }
-                       else {
-                           loadingDialog.dismiss();
-                           Toast.makeText(QuestionsActivity.this,"questions does not exists",Toast.LENGTH_SHORT).show();
-                       }
+                        }
+                        else {
+                            loadingDialog.dismiss();
+                            Toast.makeText(QuestionsActivity.this,"questions does not exists",Toast.LENGTH_SHORT).show();
+                        }
                     }
 
                     @Override
@@ -132,32 +132,30 @@ private Dialog loadingDialog;
     }
 
     private void startTimer() {
-        long time=questionTime*60*100;
-        timer=new CountDownTimer(time+1000,1000) {
+        long time = questionTime * 60 * 100;
+        timer = new CountDownTimer(time + 1000, 1000) {
             @Override
             public void onTick(long l) {
-                timeLeft=l;
-                String remainingTime=String.format("%02d:%02d min",
+                timeLeft = l;
+                String remainingTime = String.format("%02d:%02d min",
                         TimeUnit.MILLISECONDS.toMinutes(l),
-                        TimeUnit.MILLISECONDS.toSeconds(l)-
+                        TimeUnit.MILLISECONDS.toSeconds(l) -
                                 TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(l)));
                 binding.time.setText(remainingTime);
-
             }
 
             @Override
             public void onFinish() {
-
-                long totalTime=questionTime*60*100;
-                Intent intent=new Intent(QuestionsActivity.this,ScoreActivity.class);
-                intent.putExtra("time_taken",totalTime-timeLeft);
-                intent.putExtra("correct",correctAnsw);
-                intent.putExtra("wrong",wrongAnsw);
-                intent.putExtra("total_questions",list.size());
+                long totalTime = questionTime * 60 * 100;
+                Intent intent = new Intent(QuestionsActivity.this, ScoreActivity.class);
+                intent.putExtra("time_taken", totalTime - timeLeft);
+                intent.putExtra("correct", correctAnsw);
+                intent.putExtra("wrong", wrongAnsw);
+                intent.putExtra("total_questions", list.size());
                 startActivity(intent);
                 QuestionsActivity.this.finish();
             }
-        };
+        }.start();  // Start the timer here
     }
 
     private void enableOption(boolean enable) {
@@ -176,23 +174,23 @@ private Dialog loadingDialog;
                 .setInterpolator(new DecelerateInterpolator()).setListener(new Animator.AnimatorListener() {
                     @Override
                     public void onAnimationStart(@NonNull Animator animator) {
-                      if(value==0 && count<4){
-                          String option="";
-                          if(count==0){
-                              option=list.get(position).getOptionA();
-                          }
-                          else if(count==1){
-                              option=list.get(position).getOptionB();
-                          }
-                          else if(count==2){
-                              option=list.get(position).getOptionC();
-                          }
-                          else if(count==3){
-                              option=list.get(position).getOptionD();
-                          }
-                          playAnimation(binding.optionContainer.getChildAt(count),0,option);
-                          count++;
-                      }
+                        if(value==0 && count<4){
+                            String option="";
+                            if(count==0){
+                                option=list.get(position).getOptionA();
+                            }
+                            else if(count==1){
+                                option=list.get(position).getOptionB();
+                            }
+                            else if(count==2){
+                                option=list.get(position).getOptionC();
+                            }
+                            else if(count==3){
+                                option=list.get(position).getOptionD();
+                            }
+                            playAnimation(binding.optionContainer.getChildAt(count),0,option);
+                            count++;
+                        }
                     }
 
                     @Override
