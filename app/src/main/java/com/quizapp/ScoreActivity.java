a654b5ef844121f030f738acc7a308888530b380
Saadmehmood1234 <158064428+Saadmehmood1234@users.noreply.github.com>
2024-10-29 18:27:19 +0530

    added

--- app/src/main/java/com/quizapp/ScoreActivity.java
+++ app/src/main/java/com/quizapp/ScoreActivity.java
@@ -1,6 +1,8 @@
 package com.quizapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,13 +10,49 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class ScoreActivity extends AppCompatActivity {
+import com.quizapp.databinding.ActivityScoreBinding;
 
+import java.util.concurrent.TimeUnit;
+
+public class ScoreActivity extends AppCompatActivity {
+    ActivityScoreBinding binding;
+    private long timeTaken;
+    private int totalQuestions,correctAnsw,wrongAnsw,skipQuestion;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_score);
+        binding=ActivityScoreBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        totalQuestions=getIntent().getIntExtra("total_questions",0);
+        correctAnsw=getIntent().getIntExtra("correct",0);
+        wrongAnsw=getIntent().getIntExtra("wrong",0);
+
+
+        String remainingTime = String.format("%02d:%02d min",
+                TimeUnit.MILLISECONDS.toMinutes(timeTaken),
+                TimeUnit.MILLISECONDS.toSeconds(timeTaken) -
+                        TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(timeTaken)));
+
+        binding.timeTaken.setText(remainingTime);
+
+        skipQuestion=totalQuestions-correctAnsw+wrongAnsw;
+        binding.question.setText(totalQuestions+"");
+        binding.txtcorect.setText(correctAnsw+"");
+        binding.txtWrong.setText(wrongAnsw+"");
+        binding.txtSkip.setText(skipQuestion+"");
+
+        binding.score.setText(correctAnsw+"");
+
+        binding.btnReAttempt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(ScoreActivity.this,MainActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent);
+            }
+        });
+
 
     }
 }
